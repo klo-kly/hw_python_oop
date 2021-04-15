2d54e6eaafb3ea393639764932954016bcf07fd1
Калин <kalin.savelev07@gmail.com>
2021-04-15 21:59:11 +0300

    Ok

--- homework.py
+++ homework.py
@@ -1,5 +1,7 @@
 import datetime as dt
 
+from easy_date import convert_from_string
+
 
 class Calculator:
     def __init__(self, limit):
@@ -10,12 +12,11 @@ class Calculator:
         self.records.append(record)
 
     def get_today_stats(self):
-        today = dt.datetime.now().date()
-        today_stats = 0
-        for i in self.records:
-            if today == i.date:
-                today_stats += i.amount
-        return today_stats
+        today = dt.date.today()
+        return (
+            self.limit
+            - sum([i.amount for i in self.records if today == i.date])
+        )
 
     def show_records(self):
         s = ''
@@ -26,13 +27,10 @@ class Calculator:
 
     def get_week_stats(self):
         week = dt.timedelta(7)
-        today = dt.datetime.now().date()
-        week_stats = 0
-        for i in self.records:
-            date_in_record = i.date
-            if (today >= date_in_record > today - week):
-                week_stats += i.amount
-        return week_stats
+        today = dt.date.today()
+        return sum(
+            [i.amount for i in self.records if today >= i.date > today - week]
+        )
 
 
 class CashCalculator(Calculator):
@@ -43,68 +41,61 @@ class CashCalculator(Calculator):
 
         cash_remained = 0
 
-        if currency == 'rub':
-            cash_remained = self.get_today_stats()
-            limit = self.limit
-            currency = 'руб'
-        elif currency == 'usd':
-            cash_remained = self.get_today_stats() / self.USD_RATE
-            limit = self.limit / self.USD_RATE
-            currency = 'USD'
-        elif currency == 'eur':
-            cash_remained = self.get_today_stats() / self.EURO_RATE
-            limit = self.limit / self.EURO_RATE
-            currency = 'Euro'
+        currency_dict = {
+            'rub': (1, 'руб'),
+            'usd': (self.USD_RATE, 'USD'),
+            'eur': (self.EURO_RATE, 'Euro')
+        }
 
-        if self.limit > self.get_today_stats():
-            rest_of_the_limit = round(limit - cash_remained, 2)
-            return (f'На сегодня осталось {rest_of_the_limit} {currency}')
+        cash_remained = round(
+            self.get_today_stats() / currency_dict[currency][0], 2
+        )
+
+        if self.limit > - self.get_today_stats() - self.limit:
+            return ('На сегодня осталось '
+                    f'{cash_remained} {currency_dict[currency][1]}')
         elif self.limit == self.get_today_stats():
             return 'Денег нет, держись'
         else:
-            rest_of_the_limit = round(cash_remained - limit, 2)
             return ('Денег нет, держись: твой долг - '
-                    + f'{rest_of_the_limit} {currency}')
+                    f'{- cash_remained} {currency_dict[currency][1]}')
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         if self.limit > self.get_today_stats():
-            rest_of_the_limit = self.limit - self.get_today_stats()
             return ('Сегодня можно съесть что-нибудь ещё, '
-                    + 'но с общей калорийностью '
-                    + f'не более {rest_of_the_limit} кКал')
+                    'но с общей калорийностью '
+                    f'не более {self.get_today_stats()} кКал')
         else:
             return 'Хватит есть!'
 
 
 class Record:
-    def __init__(self, amount, comment, date=''):
+
+    def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
-        self.date = dt.datetime.now().date()
+        self.date = dt.date.today()
         if date:
-            date = dt.datetime.strptime(date, '%d.%m.%Y')
-            date = dt.datetime.strftime(date, '%Y-%m-%d')
-            date = dt.datetime.strptime(date, '%Y-%m-%d')
-            self.date = date.date()
+            date = convert_from_string(date, '%d.%m.%Y', '%Y-%m-%d', date)
+            self.date = date
 
     def __str__(self):
         return f'{self.amount} | {self.comment} | {self.date}'
 
 
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
+if __name__ == '__main__':
+    # создадим калькулятор денег с дневным лимитом 1000
+    cash_calculator = CashCalculator(1000)
 
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='01.11.2019'))
+    # дата в параметрах не указана,
+    # так что по умолчанию к записи
+    # должна автоматически добавиться сегодняшняя дата
+    cash_calculator.add_record(Record(amount=145, comment='кофе'))
+    # и к этой записи тоже дата должна добавиться автоматически
+    cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+    # а тут пользователь указал дату, сохраняем её
+    cash_calculator.add_record(Record(amount=300, comment='бар в Танин др'))
 
-print(cash_calculator.get_today_cash_remained('rub'))
+    print(cash_calculator.get_today_cash_remained('usd'))
